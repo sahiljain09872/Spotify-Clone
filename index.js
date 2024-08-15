29903cc658c86b14a8b9dfcad7b4695c8362f17f
Sahil Jain <153418943+sahiljain2173@users.noreply.github.com>
2024-08-15 23:19:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const { name } = require("ejs");
+const express = require("express")
+const app = express()
+const path = require("path")
+
+const port = 3000;
+
+app.set("views" , path.join(__dirname , "views"));
+app.set("view engine" , "ejs")
+app.use(express.static(path.join(__dirname , "/public/css")));
+app.use(express.static(path.join(__dirname , "/public/js")));
+app.use("/assets"  , express.static(path.join(__dirname , "/assets")));
+app.use("/songs"  , express.static(path.join(__dirname , "/songs")));
+
+
+
+app.listen( port , () =>{
+    console.log(`Server is listening to the port : ${port}`)
+})
+
+app.get(["/" , "/home" , "/index"] , (req ,res) => {
+    const data = {
+        name : "home-content"
+    }
+    res.render("project.ejs" , {data : data})
+})
+
+app.get("/search" , (req ,res)=>{
+    const data = {
+        name : "search-content"
+    }
+
+    res.render("project.ejs" , {data : data})
+})
